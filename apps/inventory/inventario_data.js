269a0210242e90091ee05df43efbf4ffb0a14fc0
Josue De Jesus <josue@linkaform.com>
2022-07-19 10:08:24 -0500

    Creación de reporte de inventario

---
+++ apps/inventory/inventario_data.js
@@ -0,0 +1,9 @@
+//----Total
+
+var columsTable1 = [{ 
+  title:"Nombre Cliente", field:'nombre_cliente', hozAlign:"left", width:400},
+  { title:"Codigo" , field:'codigo', hozAlign:"left", width:250},
+  { title:"Nombre Equipo", field:'nombre_equipo', hozAlign:"left", width:400},
+  { title:"Cantidad", field:'cantidad', hozAlign:"left", width:200},
+];
+
