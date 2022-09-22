d5f7f101cc836f76432233335b6771a4dbdcecc2
Josue De Jesus <josue@linkaform.com>
2022-09-22 18:05:28 -0500

    Creación de front de magnolia

---
+++ apps/tissueculture/age_data.js
@@ -0,0 +1,103 @@
+// Datos demo para Reporte ENcuestas MOntaje
+
+//--Table Montajes Por Mes
+
+var columsTable1 = [
+  { title:"Plant Code", field:'plant_code',hozAlign:"left",width:250},
+  { title:"Plant Name", field:'plant_name',hozAlign:"left",width:250},
+  { title:"Stage ", field:'stage',hozAlign:"rigth",width:250},
+  {title:"Cycle Age", hozAlign:"center",
+    columns:[
+      {title:"1-7 Days", field:'days_first',hozAlign:"right",width:250},
+      {title:"8-28 Days", field:'days_second',hozAlign:"right",width:250},
+      {title:"29-56 Days", field:'days_third',hozAlign:"right",width:250},
+      {title:"Over 56 Days", field:'days_fourth',hozAlign:"right",width:250},
+    ]
+  },
+];
+
+var dataTable1 = [
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    days_first:  "500",
+    days_second: "400",
+    days_third:  "300",
+    days_fourth: "200",
+  },
+  
+];
+
---
+++ apps/tissueculture/production_data.js
@@ -0,0 +1,89 @@
+// Datos demo para Reporte ENcuestas MOntaje
+
+//--Table Montajes Por Mes
+
+var columsTable1 = [
+  { title:"Plant Code", field:'plant_code',hozAlign:"left",width:250},
+  { title:"Plant Name", field:'plant_name',hozAlign:"left",width:250},
+  { title:"Stage", field:'stage',hozAlign:"left",width:250},
+  { title:"Required", field:'required',hozAlign:"left",width:250},
+  { title:"Assigned", field:'produced',hozAlign:"left",width:250},
+  { title:"Variance", field:'variance',hozAlign:"left",width:250},
+];
+
+var dataTable1 = [
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    required: "500",
+    produced: "300",
+    variance: "100",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    required: "500",
+    produced: "300",
+    variance: "100",
+  },
+  {
+    plant_code: "LNAFP",
+    plant_name: "Firepower' Nandina",
+    stage: "2",
+    required: "500",
+    produced: "300",
+    variance: "100",
+  },
+  {
+    plant_code: "LNAFP",
+    plant_name: "Firepower' Nandina",
+    stage: "2",
+    required: "500",
+    produced: "600",
+    variance: "100",
+  },
+  {
+    plant_code: "LNAFP",
+    plant_name: "Firepower' Nandina",
+    stage: "2",
+    required: "500",
+    produced: "600",
+    variance: "100",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    required: "500",
+    produced: "600",
+    variance: "100",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    required: "500",
+    produced: "600",
+    variance: "100",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    required: "500",
+    produced: "600",
+    variance: "100",
+  },
+  {
+    plant_code: "LAGBG",
+    plant_name: "Blue Glow Agave",
+    stage: "2",
+    required: "500",
+    produced: "600",
+    variance: "100",
+  },
+  
+];
+
