38fcbb4e08bb3eb9223eedec7a3619bbdf92a6ab
Josue De Jesus <josue@linkaform.com>
2022-10-14 12:27:39 -0500

    Creación de Reporte Requires By Stage

---
+++ apps/tissueculture/account_data.js
@@ -0,0 +1,138 @@
+// Datos demo para Reporte ENcuestas MOntaje
+
+//--Table Montajes Por Mes
+
+var columsTable1 = [
+  { title:"Stage", field:'stage',hozAlign:"left", frozen:true,width:250},
+  {title:"January", field:'jan',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"February", field:'feb',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"March", field:'mar',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"April", field:'apr',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"May", field:'may',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"June", field:'jun',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"July", field:'jul',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"August", field:'aug',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"September", field:'sep',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"October", field:'oct',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"November", field:'nov',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+  {title:"December", field:'dec',hozAlign:"right",formatter: "money",formatterParams: {symbol: "", symbolAfter: "", decimal: ".", thousand: ",", precision: 0},width:250},
+];
+
+
+
+var dataTable1 = [
+  {
+    stage:"stage1", 
+    _children: [
+      {
+        jan:20000,
+        feb:20000,
+        mar:20000,
+        apr:20000,
+        may:20000,
+        jun:20000,
+        jul:20000,
+        aug:20000,
+        sep:20000,
+        oct:20000,
+        nov:20000,
+        dec:20000,
+      },
+    ]
+  },
+  {
+    stage:"stage2", 
+    _children: [
+      {
+        jan:20000,
+        feb:20000,
+        mar:20000,
+        apr:20000,
+        may:20000,
+        jun:20000,
+        jul:20000,
+        aug:20000,
+        sep:20000,
+        oct:20000,
+        nov:20000,
+        dec:20000,
+      },
+    ]
+  },
+  {
+    stage:"stage3", 
+    _children: [
+      {
+        jan:20000,
+        feb:20000,
+        mar:20000,
+        apr:20000,
+        may:20000,
+        jun:20000,
+        jul:20000,
+        aug:20000,
+        sep:20000,
+        oct:20000,
+        nov:20000,
+        dec:20000,
+      },
+    ]
+  },
+  {
+    stage:"stage4", 
+    _children: [
+      {
+        jan:20000,
+        feb:20000,
+        mar:20000,
+        apr:20000,
+        may:20000,
+        jun:20000,
+        jul:20000,
+        aug:20000,
+        sep:20000,
+        oct:20000,
+        nov:20000,
+        dec:20000,
+      },
+    ]
+  },
+  {
+    stage:"stage5", 
+    _children: [
+      {
+        jan:20000,
+        feb:20000,
+        mar:20000,
+        apr:20000,
+        may:20000,
+        jun:20000,
+        jul:20000,
+        aug:20000,
+        sep:20000,
+        oct:20000,
+        nov:20000,
+        dec:20000,
+      },
+    ]
+  },
+  {
+    stage:"stage6", 
+    _children: [
+      {
+        jan:20000,
+        feb:20000,
+        mar:20000,
+        apr:20000,
+        may:20000,
+        jun:20000,
+        jul:20000,
+        aug:20000,
+        sep:20000,
+        oct:20000,
+        nov:20000,
+        dec:20000,
+      },
+    ]
+  },
+];
