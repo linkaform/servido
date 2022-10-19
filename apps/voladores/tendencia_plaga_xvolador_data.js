bd451705433fee7389f28a52d30962293dd575d9
Josue De Jesus <josue@linkaform.com>
2022-10-19 12:51:36 -0500

    Creación de reportes de Avomex

---
+++ apps/voladores/tendencia_plaga_xvolador_data.js
@@ -0,0 +1,30 @@
+// Datos demo para Reporte ENcuestas MOntaje
+
+//--Table Montajes Por Mes
+var columsTable1 = [
+  { title:"No. Dispositivo", field:'num_dispositivo',hozAlign:"left",width:250},
+  { title:"Folio", field:'folio',hozAlign:"left",width:250},
+
+  { title:"Folio", field:'folio',hozAlign:"left",width:250},
+  { title:"Fecha Volador", field:'fecha_volador',hozAlign:"left",width:250},
+  { title:"Año Volador", field:'anio_volador',hozAlign:"left",width:250},
+  { title:"Limpieza Volador", field:'limpieza_volador',hozAlign:"left",width:250},
+  { title:"Cambio Goma", field:'cambio_goma',hozAlign:"left",width:250},
+  { title:"Obs Volador", field:'obs_volador',hozAlign:"left",width:450},
+  { title:"Catalogo Planta", field:'catalogo_planta',hozAlign:"left",width:250},
+];
+
+var dataTable1 = [
+  {
+    num_dispositivo:"1",
+    obs_volador:"Example",
+    insectos:"Example",
+    volador:"Example",
+    meses:"Example",
+    anio_volador:"2022",
+    otro_volador:"Example",
+    folio:"2021-01",
+    limpieza_volador:"Example",
+    cambio_goma:"Example",
+  },
+];
