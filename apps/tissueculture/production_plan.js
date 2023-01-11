dab9ea9a8c8dbe1e0f51fd380a402739f4cb7534
Josue De Jesus <josue@linkaform.com>
2023-01-10 21:40:01 -0600

    Creación de reporte de Demo magnolia

---
+++ apps/tissueculture/production_plan.js
@@ -0,0 +1,273 @@
+// Reporte Production Forscast
+// Librerias: Chart.js
+
+let us = null;
+let usTy = null;
+let jw = null;
+let userId = null;
+let userJwt = null;
+let userName = null;
+let userParentId = null;
+let scriptId = null;
+
+$('#divOptions').hide();
+$('#title_report').hide();
+$('.title_tables').hide();
+hideElement("title_demo");
+hideElement("firstParameters");
+hideElement("firstElement");
+hideElement("secondElement");
+hideElement("thirdElement");
+
+window.onload = function(){
+  var qs = urlParamstoJson();
+  var formNode = document.getElementById("appCont");
+	for(var key in qs){
+    if (key === 'script_id' ){
+      console.log('script id', key)
+      scriptId = parseInt(qs[key]);
+    }
+    if (key === 'env') {
+      if (qs[key] === 'test'){
+         url = "https://preprod.linkaform.com/api/";
+      }
+    }
+    if (key ==='title'){
+      $("#title_report").text(qs[key]);
+    }
+		var elements = getAllElementsWithAttribute(formNode, 'data-infosync-id', key);
+		var value = decodeURI(qs[key]);
+    if (key === 'infosyncRecordID'){
+      var recId = document.getElementById("infosyncRecordID");
+      recId.value = value;
+    }
+		else if(elements.length > 0){
+			switch(elements[0].type){
+				case 'text':
+					elements[0].value = value;
+					break;
+				case 'textarea':
+					elements[0].value = value;
+					break;
+				case 'select-one':
+					elements[0].value = value;
+					break;
+				case 'radio':
+					for(var idx in elements){
+						if(elements[idx].value === value){
+							elements[idx].checked = true;
+						}
+					}
+					break;
+				case 'checkbox':
+					var values = value.split(';');
+					for(var idx in elements){
+						if(values.indexOf(elements[idx].value) !== -1){
+							elements[idx].checked = true;
+						}
+					}
+					break;
+			}
+		}
+	}
+
+  us = getCookie("userId");
+  jw = getCookie("userJwt");
+  userParentId = getCookie("userParentId");
+  hideElement("close_sesion");
+  hideElement("firstParameters");
+
+
+  if(us != "" && jw != "" || scriptId===null){
+    hideElement("inicio_ses");
+    unhideElement("close_sesion");
+    getCompanyLogo(userParentId);
+    userId = us;
+    userJwt = jw;
+    userName = getCookie("userName");
+    document.getElementById("firstParameters").style.removeProperty('display');
+    unHideReportElements()
+    if (scriptId == null) {
+      loadDemoData();
+    }
+    //--Styles
+
+    setSpinner();
+    $('#divOptions').show();
+    $('#title_report').show();
+    $("#week").multipleSelect('refresh');
+
+    document.getElementById("firstParameters").style.removeProperty('display');
+    
+  } else {
+    unhideElement("inicio_ses");
+    $('#divContent').hide();
+    $('#divOptions').hide();
+    $('#title_report').hide();
+    $('.title_tables').hide();
+    hideElement("firstElement-Buttons");
+  }
+  ///-----HIDE AND SHOW
+  for(var key in qs){
+    if (key === 'embed'){
+      if (qs[key]){
+        $("#close_sesion").hide();
+        $("#image_log").hide();
+      }
+    }
+  }
+}
+
+
+function unHideReportElements(){
+  //Set here all report elements that need to be unHiden on a loggin
+  unhideElement("firstElement-Buttons");
+  unhideElement("firstParameters");
+  unhideElement("close_sesion");
+  unhideElement("firstElement");
+}
+
+function loadDemoData(){
+
+  $('.title_tables').show();
+  unhideElement("title_demo")
+  document.getElementById("firstParameters").style.removeProperty('display');
+  
+  getDrawTable('firstElement', columsTable1, dataTable1);
+  document.getElementById("firstElement").style.removeProperty('display');
+
+  getDrawGraphicFirst(data1, setOptions1);
+  document.getElementById("secondElement").style.removeProperty('display');
+
+  getDrawGraphicSecond(data2, setOptions2);
+  document.getElementById("thirdElement").style.removeProperty('display');
+
+}
+
+const loading = document.querySelector('.loading-container');
+loading.style.display = 'none';
+
+function runFirstElement(){
+  let date_from = document.getElementById("date_from");
+  let date_to = document.getElementById("date_to");  
+  getFirstElement(date_to.value, date_from.value);
+};
+
+function getFirstElement(dateTo, dateFrom){
+  //----Hide Css
+  $("#divContent").hide();
+  $('.load-wrapp').show();
+  $('.title_tables').hide();
+
+
+  fetch(url + 'infosync/scripts/run/', {
+    method: 'POST',
+    body: JSON.stringify({
+      script_id: scriptId,
+      date_to: dateTo,
+      date_from: dateFrom,
+      servicio: servicio,
+      cliente: cliente,
+      tecnico: tecnico,
+    }),
+    headers:{
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+userJwt
+    },
+  })
+  .then(res => res.json())
+  .then(res => {
+    if (res.success) {
+      //----Hide and show
+      $('.load-wrapp').hide();
+      $("#divContent").show();
+      $('.title_tables').show();
+      if (res.response.json.firstElement.data) {
+        console.log('drawFirstElement.........');
+        getDrawTable('firstElement', columsTable1, res.response.json.firstElement.data);
+      }
+      
+    } else {
+      hideLoading();
+      if(res.code == 11){
+        Swal.fire({
+          title: 'Error',
+          html: res.error
+        });
+        $('.load-wrapp').hide();
+      } else {
+        Swal.fire({
+          title: 'Error',
+          html: res.error
+        });
+        $('.load-wrapp').hide();
+      }
+    }
+  })
+};
+
+//-----TABLES
+function getDrawTable(id, columnsData, tableData){
+  var  table = new Tabulator("#" + id, {
+    height:"350px",
+    layout:"fitDataTable",
+    data:tableData,
+    resizableRows:false,
+    dataTree:true,
+    dataTreeStartExpanded:false,
+    clipboard:true,
+    clipboardPasteAction:"replace",
+    textDirection:"ltr",
+    columns:columnsData,
+  });
+
+  if (document.getElementById("download_xlsx_"+id)){
+    //trigger download of data.xlsx file
+    document.getElementById("download_xlsx_"+id).replaceWith(document.getElementById("download_xlsx_"+id).cloneNode(true));
+    document.getElementById("download_xlsx_"+id).addEventListener("click", function (){
+    table.download("xlsx", "data.xlsx", {sheetName:"data"});
+    });
+  }
+
+  if (document.getElementById("download_csv_"+id)){
+    //trigger download of data.csv file
+    document.getElementById("download_csv_"+id).replaceWith(document.getElementById("download_csv_"+id).cloneNode(true));
+    document.getElementById("download_csv_"+id).addEventListener("click", function (){
+      table.download("csv", "data.csv");
+    });
+  }
+}
+
+//-----GRAPICH
+let chart1;
+function getDrawGraphicFirst(data, setOptions){
+  //---CHART
+  var ctx = document.getElementById('graphicFirst').getContext('2d');
+  
+  if (chart1) {
+    chart1.destroy();
+  }
+
+  chart1 = new Chart(ctx, {
+    type: 'bar',
+    data:data,
+    options: setOptions,
+  });
+}
+
+
+let chart2;
+function getDrawGraphicSecond(data, setOptions){
+  //---CHART
+  var ctx = document.getElementById('graphicSecond').getContext('2d');
+  
+  if (chart2) {
+    chart2.destroy();
+  }
+
+  chart2 = new Chart(ctx, {
+    type: 'pie',
+    data:data,
+    options: setOptions,
+  });
+}
---
+++ apps/tissueculture/production_plan_data.js
@@ -0,0 +1,262 @@
+var array_background = getPAlleteColors(4,8);
+
+//--Table Montajes Por Mes
+var columsTable1 = [
+  { title:"Plant Code", field:'plant_code',hozAlign:"left",width:150},
+  { title:"Plant Name", field:'plant_name',hozAlign:"left",width:350},
+  { title:"Container Type", field:'container_type',hozAlign:"left",width:250},
+  { title:"Week", field:'week',hozAlign:"left",width:100},
+  { title:"Status", field:'status',hozAlign:"left",width:250},
+  { title:"Production Plan", field:'production_plan',hozAlign:"left",width:200},
+  { title:"Produced Production", field:'produced_production',hozAlign:"left",width:200},
+  { title:"Progress %",  field:"progress", hozAlign:"left", formatter:"progress", editor:true, width:200 },
+
+];
+
+
+
+var dataTable1 = [
+  {
+    plant_code: "LNAFP",
+    plant_name: "Nandina domestica nana ‘Firepower’",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Planning",
+    production_plan: "850",
+    produced_production: "340",
+    progress: "40",
+  },
+    {
+    plant_code: "LNAPE",
+    plant_name: "Nandina domestica Cool Glow Peach",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Execute",
+    production_plan: "1000",
+    produced_production: "600",
+    progress: "60",
+  },
+    {
+    plant_code: "LNAPO",
+    plant_name: "Nandina domestica Cool Glow Pomegranate",
+    container_type: "Magenta Box",
+    week: "2",
+    status: "Planning",
+    production_plan: "900",
+    produced_production: "450",
+    progress: "50",
+  },
+  {
+    plant_code: "LNAFP",
+    plant_name: "Nandina domestica nana ‘Firepower’",
+    container_type: "Magenta Box",
+    week: "2",
+    status: "Planning",
+    production_plan: "850",
+    produced_production: "340",
+    progress: "40",
+  },
+  {
+    plant_code: "LNAPE",
+    plant_name: "Nandina domestica Cool Glow Peach",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Execute",
+    production_plan: "1000",
+    produced_production: "600",
+    progress: "60",
+  },
+  {
+    plant_code: "LNAPO",
+    plant_name: "Nandina domestica Cool Glow Pomegranate",
+    container_type: "Magenta Box",
+    week: "2",
+    status: "Planning",
+    production_plan: "900",
+    produced_production: "450",
+    progress: "50",
+  },
+  {
+    plant_code: "LNAFP",
+    plant_name: "Nandina domestica nana ‘Firepower’",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Planning",
+    production_plan: "850",
+    produced_production: "340",
+    progress: "40",
+  },
+  {
+    plant_code: "LNAPE",
+    plant_name: "Nandina domestica Cool Glow Peach",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Execute",
+    production_plan: "1000",
+    produced_production: "600",
+    progress: "60",
+  },
+  {
+    plant_code: "LNAPO",
+    plant_name: "Nandina domestica Cool Glow Pomegranate",
+    container_type: "Magenta Box",
+    week: "2",
+    status: "Planning",
+    production_plan: "900",
+    produced_production: "450",
+    progress: "50",
+  },
+    {
+    plant_code: "LNAFP",
+    plant_name: "Nandina domestica nana ‘Firepower’",
+    container_type: "Magenta Box",
+    week: "2",
+    status: "Planning",
+    production_plan: "850",
+    produced_production: "340",
+    progress: "40",
+  },
+    {
+    plant_code: "LNAPE",
+    plant_name: "Nandina domestica Cool Glow Peach",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Execute",
+    production_plan: "1000",
+    produced_production: "600",
+    progress: "60",
+  },
+    {
+    plant_code: "LNAPO",
+    plant_name: "Nandina domestica Cool Glow Pomegranate",
+    container_type: "Magenta Box",
+    week: "2",
+    status: "Planning",
+    production_plan: "900",
+    produced_production: "450",
+    progress: "50",
+  },
+    {
+    plant_code: "LNAFP",
+    plant_name: "Nandina domestica nana ‘Firepower’",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Planning",
+    production_plan: "850",
+    produced_production: "340",
+    progress: "40",
+  },
+    {
+    plant_code: "LNAPE",
+    plant_name: "Nandina domestica Cool Glow Peach",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Execute",
+    production_plan: "1000",
+    produced_production: "600",
+    progress: "60",
+  },
+    {
+    plant_code: "LNAPO",
+    plant_name: "Nandina domestica Cool Glow Pomegranate",
+    container_type: "Baby Jar",
+    week: "2",
+    status: "Planning",
+    production_plan: "900",
+    produced_production: "450",
+    progress: "50",
+  }, 
+];
+
+//-----DATA GRAPHIC
+
+
+
+var data1 = {
+  labels: ['LNAFP','LNAPE','LNAFP','LNAPE','LNAPO',],
+  datasets: [
+    {
+      label: 'Production Plan',
+      data: [25,10,20,15,10],
+      backgroundColor: array_background[3],
+    },
+    {
+      label: 'Produced Production ',
+      data: [10,5,10,15,10],
+      backgroundColor: array_background[0],
+    },
+  ]
+};
+
+var data2 = {
+  labels: ['Produced Production','Production Plan',],
+  datasets: [
+    {
+      label: 'Value',
+      data: [40,50],
+      backgroundColor: [array_background[3],array_background[0]],
+    },
+  ]
+};
+
+
+//-----CONFIG GRAPHIC
+var setOptions1 = {
+  plugins: {
+
+    datalabels: {
+      display: true,
+      color: 'white',
+    },
+    title: {
+      display: true,
+      text: 'Production Plant',
+      font: {
+        size: 25
+      }
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: false,
+    },
+    y: {
+      stacked: false,
+      ticks: {
+        stepSize: 1
+      },
+    }
+  }
+};
+
+var setOptions2 = {
+  plugins: {
+    datalabels: {
+      display: true,
+      color: 'white',
+    },
+    title: {
+      display: true,
+      text: 'Production Plan Vs Produced',
+      font: {
+        size: 25
+      }
+    },
+  },
+  responsive: true,
+  scales: {
+
+    x: {
+      stacked: false,
+      display: false,
+    },
+    y: {
+      stacked: false,
+      display: false,
+      ticks: {
+        stepSize: 1
+      },
+    }
+  }
+};
