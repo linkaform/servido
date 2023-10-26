febfdc0045c74bec0a68ca1750f9859f9a9dcd1b
IsraelReyes <israel@linkaform.com>
2023-10-26 15:56:03 -0600

    Actualización en la tabla del reporte de Harvest de Magnolia

--- apps/tissueculture/harvest_data.js
+++ apps/tissueculture/harvest_data.js
@@ -2,12 +2,13 @@ var array_background = getPAlleteColors(4,8);
 
 //--Table Montajes Por Mes
 var columsTable1 = [
-  { title:"Harvest Year", field:'havest_year',hozAlign:"left",width:150},
-  { title:"Harvest Month", field:'havest_month',hozAlign:"left",width:150},
-  { title:"Harvest Week", field:'havest_week',hozAlign:"left",width:150},
-  { title:"Plant Code", field:'plant_code',hozAlign:"left",width:150},
-  { title:"Plant Name", field:'plant_name',hozAlign:"left",width:250},
-  { title:"Total Harvest", field:'total_harvest',hozAlign:"left",formatterParams: {decimal: ".", thousand: ",", precision: 2 }, width:150},
+  { title:"Harvest Year", field:'havest_year',hozAlign:"left",width:250, headerFilter:"input"},
+  { title:"Harvest Month", field:'havest_month',hozAlign:"left",width:250, headerFilter:"input"},
+  { title:"Harvest Week", field:'havest_week',hozAlign:"left",width:250, headerFilter:"input"},
+  { title:"Plant Code", field:'plant_code',hozAlign:"left",width:250, headerFilter:"input"},
+  { title:"Plant Name", field:'plant_name',hozAlign:"left",width:350, headerFilter:"input"},
+  { title:"Total Harvest", field:'total_harvest',hozAlign:"right",formatterParams: {decimal: ".", thousand: ",", precision: 2 }, width:250, headerFilter:"input"},
+  { title:"From", field:'from',hozAlign:"left",width:350, headerFilter:"input"},
 ];
 
 
@@ -19,10 +20,11 @@ var dataTable1 = [
     "total": 2400, 
     "plant_name": null, 
     "total_planting": 2280.0, 
-    "havest_week": 22, 
+    "havest_week": 24, 
     "havest_month": 6, 
     "havest_year": 2023, 
-    "total_harvest": 1869.60
+    "total_harvest": 1869.60,
+    "total_harvest": ""
   }, 
     {
     "plant_code": "LNAGS", 
@@ -32,7 +34,7 @@ var dataTable1 = [
     "plant_name": null, 
     "total_planting": 2280.0, 
 
-    "havest_week": 22, 
+    "havest_week": 27, 
     "havest_month": 6, 
     "havest_year": 2023, 
     "total_harvest": 1869.60
