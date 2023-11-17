4d9c591990430b725f00c013b7d295f1e0a11374
Jose Patricio Villarreal <josepato@linkaform.com>
2023-11-17 22:00:26 +0000

    fix typo

--- servido_update.sh
+++ servido_update.sh
@@ -6,6 +6,6 @@ git checkout master
 git pull origin master
 cd apps/custom
 git submodule foreach --recursive git checkout master
-git submodule foreach --recursive git pull origin master\
+git submodule foreach --recursive git pull origin master
 cd ../..
 ./srv -p build master
