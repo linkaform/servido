d14c59a078906d1edd6f2471420e51bc53502f13
IsraelReyes <israel@linkaform.com>
2024-01-30 18:37:26 -0600

    Creación de reporte demo para documentación

---
+++ apps/tissueculture/demo_data.js
@@ -0,0 +1,73 @@
+//Datos Demo del Reporte Autitoria Sucursales
+var array_background = getPAlleteColors(11,8);
+
+//------DATA GRAPHICS
+var setOptions6 = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: true,
+      position: 'top',
+    },
+    title: {
+        display: true,
+        text: 'Reporte Historico Auditoria',
+        font: {
+          size: 25
+        }
+    },
+    datalabels: {
+      color: 'black',
+      font:{
+        size: 15,
+      }
+    }
+  },
+  scales: {
+    y: {
+      ticks: {
+        stepSize: 1
+      },
+    }
+  }
+};
+
+//Data pora el gráfico de barras
+var dataFirstElement = {
+  labels: ["Mauricio Hernández","Armando Contreras"],
+  datasets: [
+    {
+      label: "Servicios",
+      backgroundColor: ["#7BD3EA"],
+      data: [6,9],
+    },
+    {
+      label: "Adjust out",
+      backgroundColor: [ "#ECA869",],
+      data: [10,8],
+    }
+  ]
+}; 
+
+//-----DATA GAUGE
+
+var dataGauge1 = [
+  {
+    domain: { x: [0, 1], y: [0, 1] },
+    value: 80,
+    title: { text: "Balance total" , 'font': {'size': 22} },
+    type: "indicator",
+    mode: "gauge+number",
+    gauge: {
+    axis: { range: [null, 100], tickwidth: 1},
+    bar: { color: "#018088" },
+    bgcolor: "white",
+    borderwidth: 2,
+    bordercolor: "gray",
+    steps: [
+
+    { range: [0, 100], color: "#fff" }
+    ],
+    },
+  }
+];
