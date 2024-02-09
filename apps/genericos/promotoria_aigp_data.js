7123982ff89cd0e0e72c6b886f3d53d2f6c1edf3
IsraelReyes <israel@linkaform.com>
2024-02-08 20:37:29 -0600

    Cambios en la data del reporte generico de algroup

--- apps/genericos/promotoria_aigp_data.js
+++ apps/genericos/promotoria_aigp_data.js
@@ -11,7 +11,7 @@ var columsTable = [
   //{title:"Fecha", field:"fecha_registro", width:120, headerWordWrap:true},
   { title:"Usuario", field:'usuario', width:180, headerWordWrap:true},
   { title:"Fecha", field:'fecha', hozAlign:"left", formatter:"link", formatterParams:{
-    url:function(cell){return "https://preprod.linkaform.com/#/records/detail/" + cell.getData().record_id}, 
+    url:function(cell){return "https://app.linkaform.com/#/records/detail/" + cell.getData().record_id}, 
     target:"_blank",}, width:180, headerWordWrap:true},
   {title:"Ciudad", field:"ciudad",  width:250, headerWordWrap:true},
   {title:"Cadena", field:"cadena", hozAlign:"left", width:250, headerWordWrap:true},
