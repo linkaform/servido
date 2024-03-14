55b7b066ac228f846c7fcd1cca6ea18923e12f8c
IsraelReyes <israel@linkaform.com>
2024-03-14 05:45:59 -0600

    Modificación en el titulo de cada tooltip

--- apps/inspecciones/mantenimiento_gencell.js
+++ apps/inspecciones/mantenimiento_gencell.js
@@ -158,7 +158,8 @@ function runFirstElement(){
   let cliente = $("#cliente").val()
   console.log("clientes")
   console.log(cliente)  
-  
+  console.log(date_from.value)
+  console.log(date_to.value)
   getFirstElement(
     date_to.value,
     date_from.value, 
@@ -347,48 +348,21 @@ function  drawFirstElement(data){
     options: {
     responsive: true,
     plugins: {
-      legend: {
-        labels: {
-          generateLabels: function(chart) {
-            // Get the default label list
-            const original = Chart.overrides.pie.plugins.legend.labels.generateLabels;
-            const labelsOriginal = original.call(this, chart);
 
-            // Build an array of colors used in the datasets of the chart
-            let datasetColors = chart.data.datasets.map(function(e) {
-              return e.backgroundColor;
-            });
-            datasetColors = datasetColors.flat();
-
-            // Modify the color and hide state of each label
-            labelsOriginal.forEach(label => {
-              // There are twice as many labels as there are datasets. This converts the label index into the corresponding dataset index
-              label.datasetIndex = (label.index - label.index % 2) / 2;
-
-              // The hidden state must match the dataset's hidden state
-              label.hidden = !chart.isDatasetVisible(label.datasetIndex);
-
-              // Change the color to match the dataset
-              label.fillStyle = datasetColors[label.index];
-            });
-
-            return labelsOriginal;
-          }
-        },
-        onClick: function(mouseEvent, legendItem, legend) {
-          // toggle the visibility of the dataset from what it currently is
-          legend.chart.getDatasetMeta(
-            legendItem.datasetIndex
-          ).hidden = legend.chart.isDatasetVisible(legendItem.datasetIndex);
-          legend.chart.update();
-        }
-      },
       tooltip: {
         callbacks: {
+          title: function(context) {
+              console.log(context)
+              const labelIndex = (context[0].datasetIndex * 2) + context[0].dataIndex;
+              return context[0].chart.data.labels[labelIndex]
+          },
+
           label: function(context) {
+            console.log(context)
             const labelIndex = (context.datasetIndex * 2) + context.dataIndex;
             return context.chart.data.labels[labelIndex] + ': ' + context.formattedValue;
           }
+
         }
       }
     }
