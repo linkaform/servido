a0d448542a2b826d4bf3659ba810ed1a94573414
Maricruz Alvarado <maricruzalvaradoc@outlook.com>
2024-04-25 11:43:52 -0600

    cambios en pantallas rondines, turns y registro

--- apps/solucion_accesos/portal_rondines.js
+++ apps/solucion_accesos/portal_rondines.js
@@ -1,16 +1,64 @@
 
+const columsData2 = [
+    { title: "Opciones", field: "actions" , hozAlign: "left", resizable:false,width:190,
+        formatter: (cell, formatterParams) => {
+            //----Button Trash
+            let folio = cell.getData().folio ? cell.getData().folio : 0;
+            let divActions = '<div class="row d-flex">';
+            divActions += `<button class="btn-table-bitacora" onClick="setModal('Delivery',${folio})"><i class="fa-solid fa-address-card"></i></button>`;
+            divActions += `<button class="btn-table-bitacora" ><i class="fa-solid fa-print"></i></button>`;
+            divActions += '</div>';
+            return divActions;
+        },
+    },
+    { title:"Locker", field:'locker',hozAlign:"left",headerFilter:true,width:225},
+    { title:"Estatus del locker", field:'status',hozAlign:"left",headerFilter:true,width:225},
+    { title:"Visitante", field:'visit',hozAlign:"left",headerFilter:true,width:500},
+    { title:"Documento", field:'document',hozAlign:"left",headerFilter:true,width:225},
+    { title:"Número Gafete", field:'num_access',hozAlign:"left",headerFilter:true,width:225},
+    { title:"Planta", field:'location',hozAlign:"left",headerFilter:true,width:225},
+];
+
+
+
+const dataTable2 = [
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+    {'locker':'Locker 1','status':'Ocupado','visit':'Rodolfo Peña Gonzales','document':'INE','num_access':'A58','location':'PLanta 1'},
+]
+
+
+
 window.onload = function(){
     let user = getCookie("userId");
     let jw = getCookie("userJwt");
 
     if(user !='' && jw!=''){
+        drawTable('tableSalidas',columsData2,dataTable2);
     } else{
 		redirectionUrl('login',false);
 	}
 
 }
 
-
+//-----TABLES
+function drawTable(id, columnsData, tableData,){
+  var  table = new Tabulator("#" + id, {
+    layout:"fitDataTable",
+    data:tableData,
+    textDirection:"ltr",
+    columns:columnsData,
+    pagination:true, 
+    paginationSize:40,
+  });
+}
 //----Function Redirection
 function redirectionUrl(type = 'null',blank = true){
     let urlNew =  '';
--- apps/solucion_accesos/registro_ingreso.js
+++ apps/solucion_accesos/registro_ingreso.js
@@ -2,6 +2,7 @@ let urlImgCard = '';
 let urlImgUser = '';
 let flagVideoCard = false;
 let flagVideoUser = false;
+let dataCatalog='';
 
 window.onload = function(){
 	$(".select-car-register").select2({
@@ -16,6 +17,60 @@ window.onload = function(){
 	$("#selectVisit").select2({
 	  tags: true
 	});
+
+	const urlParams = new URLSearchParams(window.location.search);
+	let ubicacion = urlParams.get('ubicacion');
+	let caseta = urlParams.get('caseta');
+	document.getElementById("textLocation").textContent=ubicacion;
+	document.getElementById("textModule").textContent=caseta;
+
+	let data=[
+		{"value":"vw","label":"volkswagen"},
+		{"value":"mrcs","label":"mercedes"},
+		{"value":"mrcs","label":"mercedes"},
+		{"value":"mrcs","label":"mercedes"}
+	];
+
+	//getCatalogMarca()
+	
+	$(document).ready(function(){
+	    generateOptions(data);
+	});
+
+	function generateOptions(data){
+	var selectList = document.getElementById('selectMarca');
+
+	//Create and append the options
+	for (var i = 0; i < data.length; i++) {
+	    var option = document.createElement("option");
+	    option.value = data[i].value;
+	    option.text = data[i].label;
+	    selectList.appendChild(option);
+	 }
+	}
+
+}
+
+function getCatalogMarca(){
+	fetch(urlLinkaform, {
+		method: 'POST',
+		body: JSON.stringify({
+			script_id: 12344,
+			option: 'query_CatalogMarcas',
+		}),
+		headers:{
+	      'Content-Type': 'application/json',
+	      'Authorization': 'Bearer '+userJwt,
+	      'Access-Control-Request-Headers':'*'
+	    },
+	})
+	.then(res => res.json())
+	.then(res => {
+		if (res.success) {
+			let data = res.response.json;
+			setDataInformation('data',data)
+		} 
+	})
 }
 
 //----Function Redirection
@@ -215,13 +270,13 @@ function setRequestFileImg(type) {
 					ctx.clearRect(0, 0, canvas.width, canvas.height);
 				}
 			}else{
-				return 'Error';
 				console.log('Error aqui 2');
+				return 'Error';
 			}
 		})
 		.catch(error => {
-			return 'Error';
 			console.log('Error aqui 3');
+			return 'Error';
 		});
 	}else{
 		return 'Error';
