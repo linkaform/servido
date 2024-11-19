f12cc1ef5e4ca7ade970bfe5619438458e66888b
Josue <josue.djesus.ramirez@gmail.com>
2024-11-19 10:51:26 -0600

    Creación de reporte de Formas para LInkaform

---
+++ apps/Linkaform/reporte_formas_data.js
@@ -0,0 +1,26 @@
+
+var columsTable1 = [
+  { title:"Nombre Usuario", field:'name_user',hozAlign:"left",width:250},
+  { title:"Nombre ", field:'firstname_user',hozAlign:"left",width:250},
+  { title:"Email Usuario ", field:'email_user',hozAlign:"left",width:250},
+  { title:"Dueño", field:'status_shared',hozAlign:"left",width:100},
+];
+
+var dataTable1 =  [
+  { name_user: "User1", firstname_user: "FirstName1", email_user: "user1@example.com", status_shared: "No" },
+  { name_user: "User2", firstname_user: "FirstName2", email_user: "user2@example.com", status_shared: "No" },
+  { name_user: "User3", firstname_user: "FirstName3", email_user: "user3@example.com", status_shared: "No" },
+  { name_user: "User4", firstname_user: "FirstName4", email_user: "user4@example.com", status_shared: "No" },
+  { name_user: "User5", firstname_user: "FirstName5", email_user: "user5@example.com", status_shared: "No" },
+  { name_user: "User6", firstname_user: "FirstName6", email_user: "user6@example.com", status_shared: "No" },
+  { name_user: "User7", firstname_user: "FirstName7", email_user: "user7@example.com", status_shared: "No" },
+  { name_user: "User8", firstname_user: "FirstName8", email_user: "user8@example.com", status_shared: "No" },
+  { name_user: "User9", firstname_user: "FirstName9", email_user: "user9@example.com", status_shared: "No" },
+  { name_user: "User10", firstname_user: "FirstName10", email_user: "user10@example.com", status_shared: "No" },
+  { name_user: "User11", firstname_user: "FirstName11", email_user: "user11@example.com", status_shared: "No" },
+  { name_user: "User12", firstname_user: "FirstName12", email_user: "user12@example.com", status_shared: "No" },
+  { name_user: "User13", firstname_user: "FirstName13", email_user: "user13@example.com", status_shared: "No" },
+  { name_user: "User14", firstname_user: "FirstName14", email_user: "user14@example.com", status_shared: "Yes" },
+  { name_user: "User15", firstname_user: "FirstName15", email_user: "user15@example.com", status_shared: "No" },
+];
+
