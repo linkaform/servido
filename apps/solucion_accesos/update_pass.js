917b4e539ef06f8a48c780b7a08620942a51b428
paco soto <francisotomtz@gmail.com>
2025-02-24 10:44:38 -0600

    se agrego la peticion para activar la cuenta del usuario, y se hace login para obtener jwt y luego poder actualizar la password

--- apps/solucion_accesos/update_pass.js
+++ apps/solucion_accesos/update_pass.js
@@ -1,8 +1,15 @@
+const urlParams = new URLSearchParams(window.location.search);
+let userNameUrl = urlParams.get('userUsername');
+
 window.onload = function () {
     const newPassInput = document.getElementById('new_pass');
     const confirmPassInput = document.getElementById('confirm_pass');
+    const actualPassInput = document.getElementById('actual_pass');
     const newPassError = document.getElementById('new_pass_error');
     const confirmPassError = document.getElementById('confirm_pass_error');
+    const userNameSpan = document.getElementById('userNameSpan');
+    
+    userNameSpan.innerText = userNameUrl;
 
     newPassInput.addEventListener('input', function () {
         if (newPassInput.value.length < 6) {
@@ -24,8 +31,9 @@ window.onload = function () {
         event.preventDefault();
         const newPass = newPassInput.value;
         const confirmPass = confirmPassInput.value;
+        const actualPass = actualPassInput.value;
 
-        if (!newPass || !confirmPass) {
+        if (!newPass || !confirmPass || !actualPass) {
             Swal.fire({
                 title: 'Error',
                 text: 'Por favor, completa todos los campos.',
@@ -55,21 +63,86 @@ window.onload = function () {
             return;
         }
 
-        updatePassword(newPass, confirmPass);
+        getLoginCreds(userNameUrl, actualPass, newPass, confirmPass);
         document.getElementById('update_pass_form').reset();
+    });
+
+    activateAccount();
+};
+
+async function activateAccount(){
+    let urlActivateAccount= `https://app.linkaform.com/api/infosync/activate_user/`
+    
+    await fetch(urlActivateAccount, {
+        method: 'POST',
+        body: JSON.stringify({
+            username: userNameUrl,
+        }),
+        headers:{
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(res => {
+        if(res.status == 202){
+            console.log('Cuenta activada');
+        }else{
+            console.log('La cuenta ya se encuentra activa');
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+};
+
+function getLoginCreds(userUsername, userPassword, newPass, confirmPass){
+    loadingService();
+    let urlLogin = `https://app.linkaform.com/api/infosync/user_admin/login/`
+
+    if(userUsername !='' && userPassword!=''){
+        fetch(urlLogin, {
+            method: 'POST',
+            body: JSON.stringify({
+                username: userUsername,
+                password: userPassword,
+            }),
+            headers:{
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            if(res.code == 2){
+                let userId = res.user && res.user.id ? res.user.id : 0;
+                let userJwt = res.jwt ? res.jwt : '';
+                setCookie("userId_soter", userId, 7);
+                setCookie("userJwt_soter", userJwt, 7);
+                updatePassword(newPass, confirmPass);
+            }else{
+                Swal.fire({
+                    title: 'Error',
+                    text: res.error,
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar'
+                })
+            }
+        })
+    }else{
+        console.log('Faltan datos en login')
+    }
+};
+
+function clearCookies(nombres) {
+    nombres.forEach(nombre => {
+        document.cookie = `${nombre}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
     });
 };
 
-
 async function updatePassword(newPass, confirmPass){
-    const urlParams = new URLSearchParams(window.location.search);
-    let userId = urlParams.get('userId_soter');
-    //Falta una forma de obtener el JWT para hacer la peticion
-    let userJwt=''
-    let urlChangeImage= `https://app.linkaform.com/api/infosync/user_admin/${userId}/`
+    let userId = getCookie('userId_soter');
+    let userJwt = getCookie('userJwt_soter');
+    let urlChangePassword= `https://app.linkaform.com/api/infosync/user_admin/${userId}/`
     
-    loadingService()
-    await fetch(urlChangeImage, {
+    await fetch(urlChangePassword, {
         method: 'PATCH',
         body: JSON.stringify({
             password: newPass,
@@ -82,6 +155,7 @@ async function updatePassword(newPass, confirmPass){
     })
     .then(res => {
         if(res.status == 202){
+            clearCookies(["userId_soter", "userJwt_soter"]);
             Swal.fire({
                 title: 'Contraseña actualizada',
                 text: 'La contraseña ha sido actualizada con éxito',
