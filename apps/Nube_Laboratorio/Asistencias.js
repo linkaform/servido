8456fc9d2d3de78d76792b6699c63aba5b7cb385
Josue <josue.djesus.ramirez@gmail.com>
2025-02-28 08:35:51 -0600

    Demo para reporte de Nube laboratorio

---
+++ apps/Nube_Laboratorio/Asistencias.js
@@ -0,0 +1,81 @@
+let dataCatalogs = [];
+window.onload = function(){
+  createElements(dicReportContext);
+  setElementsStyle();
+  const statusSession = getSession();
+  if(statusSession == 'Active'){
+    loadData();
+  }else if(statusSession == 'Demo'){
+    loadDemoData();
+  }else if(statusSession == 'Offline'){
+    loadDemoData();
+  } 
+}
+
+function loadDemoData(){
+    drawTableElement('tableFirst', dataTable1, columsTable1);
+    setTimeout(() => { hide_loading();}, 2000);
+}
+
+
+//-----LOAD DATA ACTIVE
+function loadData(data) {
+    //----Search Catalogs
+    get_catalog();
+    //----Assing Events
+    const buttonExecution = document.getElementById("buttonExecution");
+    buttonExecution.addEventListener("click", () => {
+        getInformation();
+    });
+    //-----Loading
+    setTimeout(() => { hide_loading();}, 2000);
+}
+
+
+//-----SET REQUEST
+async function getInformation(){
+    showLoadingComponent();
+    const scriptId = getParameterURL('script_id');
+    const demo = getParameterURL('demo');
+    const statusSession = getSession();
+    const dicAdional = {'option':'report'}
+    if(statusSession == 'Demo' || demo){
+        Swal.fire({
+          title: 'Advertencia',
+          html: 'No es posible ejecutar el reporte, pues esta en formato demo.'
+        });
+    }else if(scriptId != null && statusSession == 'Active' && !demo){
+        const responseRequest = await sendRequestReport(scriptId,dicAdional);
+        const data = responseRequest.response && responseRequest.response.data ? responseRequest.response.data : {};
+        if(data.response_first){
+            drawTableElement('tableFirst', data.response_first, columsTable1, 'Ordenes_Checks', undefined, designPDF);
+        }
+        //-----Style
+        hideLoadingComponent();
+        showElements();
+    }
+}
+
+
+function get_catalog(){
+    const scriptId = getParameterURL('script_id');
+    const JWT = getCookie("userJwt");
+    fetch(getUrlRequest('script'), {
+        method: 'POST',
+        body: JSON.stringify({
+            script_id: scriptId,
+            option: 'filter',
+        }),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+JWT
+        },
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        const data = res.response && res.response.data ? res.response.data : [];
+        if(data.length > 0){
+            set_catalog_select(data, 'empleado', 'empleado');
+        }
+    })
+}
