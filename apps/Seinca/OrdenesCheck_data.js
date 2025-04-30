5ba90317278529643f8bc463135c127bbd61ce7a
Josue <josue.djesus.ramirez@gmail.com>
2025-04-30 08:33:09 -0600

    Modificaciones a reporte de Check In - Check OUT Para agregar nuevas columnas en la tabla

--- apps/Seinca/OrdenesCheck_data.js
+++ apps/Seinca/OrdenesCheck_data.js
@@ -39,6 +39,11 @@ let columsTable1 = [
         if(url){ window.open(url,'_blank'); }
     }, download: false, width:50},
     { title:"Check Out", field:'check_out',  hozAlign:"center", headerHozAlign: "center", headerFilter: true, width:200},
+
+    { title:"Cliente", field:'cliente', hozAlign:"left", headerHozAlign: "center",width:250},
+    { title:"Localidad", field:'localidad', hozAlign:"left", headerHozAlign: "center",width:250},
+    { title:"Supervisor", field:'supervisor', hozAlign:"left", headerHozAlign: "center",width:250},
+    
     { title:"Hrs Nocturna", field:'hrs_noct', hozAlign:"center", headerHozAlign: "center", bottomCalc:"sum", bottomCalcParams:{precision:2,}, width:200},
     { title:"Hrs Extra al 35%", field:'hrs_ext', hozAlign:"center", headerHozAlign: "center", bottomCalc:"sum", bottomCalcParams:{precision:2,}, width:200},
     { title:"Hrs Extra al 100%", field:'hrs_ext_full', hozAlign:"center", headerHozAlign: "center", bottomCalc:"sum", bottomCalcParams:{precision:2,}, width:200},
