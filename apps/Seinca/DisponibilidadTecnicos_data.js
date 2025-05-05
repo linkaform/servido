7a20224f77079c862e58ed6adb7fff9e45151bd9
Josue <josue.djesus.ramirez@gmail.com>
2025-05-05 10:11:07 -0600

    Cambio a status que definen disponible u ocupado en el reporte de Seinca de disponibilidad de tecnicos

--- apps/Seinca/DisponibilidadTecnicos_data.js
+++ apps/Seinca/DisponibilidadTecnicos_data.js
@@ -11,10 +11,10 @@ let columsTable1 = [
     { title: "Disponibilidad", field: 'disponibility', 
         formatter: function(cell) {
             let value = cell.getValue();
-            if (value == 'Salida' || value == 'Pendiente') {
+            if (value == 'Pendiente') {
                 cell.getElement().style.backgroundColor = "#82e0aa";
                 return "Disponible"; 
-            }else if(value == 'Entrada' || value == 'Registrar_entrada'){
+            }else if(value == 'Check in' || value == 'Check out'){
                 cell.getElement().style.backgroundColor = "#f5b7b1";
                 return "Ocupado"; 
             }
