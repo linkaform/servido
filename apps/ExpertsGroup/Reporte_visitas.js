0a9ab10c480fd5211024a6f36759c64287cb908a
Josue <josue.djesus.ramirez@gmail.com>
2025-06-17 16:14:57 -0600

    Creación de nuevo Reporte de visitas para experts group

---
+++ apps/ExpertsGroup/Reporte_visitas.js
@@ -0,0 +1,65 @@
+let dataCatalogs = [];
+
+window.onload = function(){
+  createElements(dicReportContext);
+  setElementsStyle();
+  const statusSession = getSession();
+  console.log('statusSession',statusSession)
+  if(statusSession == 'Active'){
+    loadData();
+  }else if(statusSession == 'Demo'){
+    loadDemoData();
+  }else if(statusSession == 'Offline'){
+    loadDemoData();
+  }
+}
+
+//-----FUNCTIONS DEMO
+function loadDemoData(){
+  //---Definitions ELements
+  drawTableElement('tableFirst', dataTable1, columsTable1);
+  drawChartElement('chartFirst','pie',dataChart1A,setOptions1A, undefined, true);
+  drawChartElement('chartSecond','bar',dataChart2A,setOptions2A, undefined, true);
+  //----TEST MAP
+  drawMapElement('mapFirst', 'Delivery progress by state' , dataMap1, configMap1)
+  setTimeout(() => { hide_loading();}, 2000);
+}
+
+
+//-----FUNCTION ACTIVE
+function loadData(data) {
+    //---Data
+    setEventsLoad();
+    //---Hide
+    setTimeout(() => { hide_loading();}, 2000);
+}
+
+//-----SET REQUEST
+async function getInformation(dicAditional){
+    const demo = getParameterURL('demo');
+    const scriptId = getParameterURL('script_id');
+    const statusSession = getSession();
+    const dicAdional =  dicAditional;
+
+    if(statusSession == 'Demo' || demo){
+        Swal.fire({
+          title: 'Advertencia',
+          html: 'No es posible ejecutar el reporte, pues esta en formato demo.'
+        });
+    }else if(scriptId != null && statusSession == 'Active' && !demo){
+        const responseRequest = await sendRequestReport(scriptId, dicAdional);
+        const data = responseRequest.response && responseRequest.response.data ? responseRequest.response.data : {};
+
+        
+        //-----Style
+        const divEmpty = document.querySelectorAll('.div-content-empty');
+        const divElements = document.querySelectorAll('.div-content-element');
+        divElements.forEach(div => {
+          div.style.visibility = 'visible';
+        });
+        divEmpty.forEach(div => {
+          div.style.display = 'none';
+        });
+    }
+}
+
