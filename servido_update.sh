0b3bd06d68008217f4aec2f2e0bc159960a4d75e
Abel Abraham Camarillo Ojeda <acamari@verlet.org>
2026-08-05 00:41:40 +0000

    error handling

--- servido_update.sh
+++ servido_update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
+set -xe
 cd ~/lkf/servido
 git checkout master
 git pull origin master
-cd apps/custom
-git submodule foreach --recursive git checkout master
-git submodule foreach --recursive git pull origin master
-cd ../..
+(
+        cd apps/custom
+        git submodule foreach --recursive git checkout master
+        git submodule foreach --recursive git pull origin master
+)
 ./srv -p build master
